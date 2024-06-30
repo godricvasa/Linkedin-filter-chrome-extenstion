15822c5ad343b90ac46e85e9e41ba5f6815523b3
vasa <vasa@fedora.tail3d82ee.ts.net>
2024-06-30 16:31:02 +0530

    added comments

--- public/background.js
+++ public/background.js
@@ -1,3 +1,4 @@
+//getting the blocklist from chrome.storage
 const getBlocklist = (callback) => {
   chrome.storage.sync.get(["blocklist"], (result) => {
     const blocklist = result.blocklist || [];
@@ -5,49 +6,12 @@ const getBlocklist = (callback) => {
   });
 };
 
-// Example usage of getBlocklist
+// logging the current blocklist
 getBlocklist((blocklist) => {
   console.log("Blocklist retrieved:", blocklist);
 });
 
-//for getting the url the user is currently in
-// const getCurrentTabUrl = (callback) => {
-//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//     if (tabs.length > 0) {
-//       const url = tabs[0].url;
-//       callback(url);
-//     } else {
-//       callback(null); // No active tab found
-//     }
-//   });
-// };
-
-// // Example usage to log the current URL
-// getCurrentTabUrl((url) => {
-//   console.log("Current URL:", url);
-//   return url;
-// });
-// // Function to check if a URL is in the blocklist
-// const isUrlBlocked = (url, callback) => {
-//   chrome.storage.sync.get(["blocklist"], (result) => {
-//     const blocklist = result.blocklist || [];
-//     const isBlocked = blocklist.some((blockedUrl) => url.includes(blockedUrl));
-//     callback(isBlocked);
-//   });
-// };
-
-// // Example usage:
-// const urlToCheck = getCurrentTabUrl;
-// isUrlBlocked(urlToCheck, (blocked) => {
-//   if (blocked) {
-//     console.log(`${urlToCheck} is blocked`);
-//   } else {
-//     console.log(`${urlToCheck} is not blocked`);
-//   }
-// });
-// Function to get current URL of active tab
-// Function to get current URL of active tab
-// Function to get current URL of active tab
+//getting the current url,the user is visiting
 const getCurrentTabUrl = (callback) => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs.length > 0) {
@@ -75,7 +39,6 @@ const logCurrentUrlAndCheckBlocked = () => {
       //   console.log("Current URL:", currentUrl);
       isUrlBlocked(currentUrl, (blocked) => {
         if (blocked) {
-          //   console.log(`${currentUrl} is blocked`);
           // Block the site by updating tab URL to blocked.html
           chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             const tabId = tabs[0].id;
@@ -93,5 +56,5 @@ const logCurrentUrlAndCheckBlocked = () => {
   });
 };
 
-// Interval to track current URL every 5 seconds
+// Interval to track current URL every second
 setInterval(logCurrentUrlAndCheckBlocked, 1000);
